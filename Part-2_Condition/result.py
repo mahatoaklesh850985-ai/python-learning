6bf48bd9e031e69cf7116243ecaffedef506595f
mahatoaklesh850985-ai <mahatoaklesh850985@gmail.com>
2026-04-02 20:04:01 +0530

    Create result.py

---
+++ Part-2_Condition/result.py
@@ -0,0 +1,12 @@
+# Result system
+
+marks = int(input("Enter marks: "))
+
+if marks >= 80:
+    print("A+")
+elif marks >= 60:
+    print("A")
+elif marks >= 50:
+    print("B")
+else:
+    print("Fail")
