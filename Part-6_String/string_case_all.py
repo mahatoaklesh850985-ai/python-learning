c2d089c382fc7639edeb854091fc1f3ad5aa311d
mahatoaklesh850985-ai <mahatoaklesh850985@gmail.com>
2026-04-02 23:29:23 +0530

    Create string_case_all.py

---
+++ Part-6_String/string_case_all.py
@@ -0,0 +1,18 @@
+# String case methods
+
+text = "hello world"
+
+# Upper case
+print("Upper:", text.upper())
+
+# Lower case
+print("Lower:", text.lower())
+
+# Capitalize (first letter বড়)
+print("Capitalize:", text.capitalize())
+
+# Title (প্রতিটা word বড়)
+print("Title:", text.title())
+
+# Casefold (advanced lower)
+print("Casefold:", text.casefold())
