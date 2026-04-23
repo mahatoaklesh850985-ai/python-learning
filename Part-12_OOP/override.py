547a0cae02af075085e53b82801e4f97528ee106
mahatoaklesh850985-ai <mahatoaklesh850985@gmail.com>
2026-04-23 11:09:11 +0530

    Create override.py

---
+++ Part-12_OOP/override.py
@@ -0,0 +1,14 @@
+# Method overriding
+
+class Animal:
+    def sound(self):
+        print("Animal sound")
+
+
+class Dog(Animal):
+    def sound(self):
+        print("Dog barks")
+
+
+d = Dog()
+d.sound()
